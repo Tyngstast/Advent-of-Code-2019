aba407c40fae7130d867e2739140bf8b195659c5
Henrik Johansson <henrj@redpill-linpro.com>
2019-12-02 18:09:40 +0100

    part 2

--- day2/program_alarm.py
+++ day2/program_alarm.py
@@ -11,8 +11,20 @@ def run():
         reader = csv.reader(f)
         data = [int(x) for x in next(reader)]
 
-        data[1] = 12
-        data[2] = 2
+        # 12 2 : 5482655
+        # 12 0 : 5482653
+
+        # idx 2 : add
+
+        # 11 0 : 5098653. 5482653 (12) - 5098653 (11) = 384000
+        # 13 0 : 5866653. 5866653 (13) - 5482653 (12) = 384000
+
+        # Wanted is 19690720. 19690720 / 384000 = 51,277916667
+        # 51 0 :    20458653
+        # 49 0 :    19690653. 19690720 - 19690653 = 67
+
+        data[1] = 49
+        data[2] = 67
 
         result = process(data)
 
@@ -28,9 +40,9 @@ def process(data):
         if opcode[0] == 99:
             break
 
-        operator = __get_operator(opcode[0])
-        res = operator(data[opcode[1]], data[opcode[2]])
-        data[opcode[3]] = res
+        op = __get_operator(opcode[0])
+        op_res = op(data[opcode[1]], data[opcode[2]])
+        data[opcode[3]] = op_res
 
     return data
 
